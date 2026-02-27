7554070f7a79538b7a294d2719921c9b09fc84b1
igorzizinio <igorschug6@gmail.com>
2026-02-27 14:48:45 -0300

    lol: add summoner.byPuuid

--- packages/lol/src/modules/SummonerModule.ts
+++ packages/lol/src/modules/SummonerModule.ts
@@ -1,5 +1,17 @@
 import type { RiotClient } from "@unrito/core";
+import { RegionalRegion } from "@unrito/core/dist/routing/regions";
 
 export class SummonerModule {
   constructor(private readonly core: RiotClient) {}
+
+  async byPuuid(puuid: string, region?: RegionalRegion) {
+    const resolvedRegion = this.core.resolveRegional(region);
+
+    return this.core
+      .getHttpClient()
+      .getRegional(
+        resolvedRegion,
+        `/lol/summoner/v4/summoners/by-puuid/${encodeURIComponent(puuid)}`,
+      );
+  }
 }
